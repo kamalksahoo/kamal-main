f23230dcd9f0dc975c252d5986313ca14451baf1
kamal kant sahoo <kamal44399@gmail.com>
2025-12-21 20:25:15 +0530

    removed unused code

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,6 +1,4 @@
 export { default as MoonIcon } from "./MoonIcon";
 export { default as SunIcon } from "./SunIcon";
 export { default as GithubIcon } from "./GithubIcon";
-export { default as LeetcodeIcon } from "./LeetcodeIcon";
 export { default as LinkedInIcon } from "./LinkedInIcon";
-export { default as XIcon } from "./XIcon";
